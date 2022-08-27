572939a2a2c57a1e5b3817dcbb7e3d780d921f13
SeanByju <sean.byju@gmail.com>
2022-08-26 21:02:26 -0400

    made min username 3 chars

--- schemas.js
+++ schemas.js
@@ -24,7 +24,7 @@ const extention = (joi)=> ({
 const Joi = BaseJoi.extend(extention);
 
 module.exports.userSchema = Joi.object({    
-    username: Joi.string().alphanum().min(6).max(30).required(),
+    username: Joi.string().alphanum().min(3).max(30).required(),
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
 }).required()
